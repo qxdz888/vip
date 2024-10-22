a764842acef0552af5c17a6a711acdb5df2ef90f
qxdz888 <161924047+qxdz888@users.noreply.github.com>
2024-10-22 19:13:27 +0800

    Update shiliuhuixuan.js

--- shiliuhuixuan.js
+++ shiliuhuixuan.js
@@ -1,29 +1,31 @@
 /**
-* @石榴会选多账号申请试用。2024-05-12
-*@sdqd6318
-*/
-const 活动ID = [   937609、824415、824414、824086、824409、824374、823385 ]；​​​​​​​​​​​​ // 修改为包含多个活动ID的队列。多个ID用逗号间隔开   
+ * @石榴会选多账号申请试用。2024-05-12
+ *@sdqd6318 
+ */
+const activityIds = [924829,924843,924833,924901,923817,923554,923556,923546,923541,923463,923398]; // 修改为包含多个活动ID的数组。多个ID用逗号间隔开
 
-// 账号储备，包含4个账号的信息
-常量账户= [
-  {    名称：“0819andy819119”，PHPSESSID：“09F3A5F9C1B3AFBD60645BD24CE8F840”，id：111264     }，
-  {   名称：“1501andy819117”，PHPSESSID：“915365fc93889d5c34143c10b9012d8e”，id：136939     }，
-  {名称：“6008ailecat”，PHPSESSID：“4c77183f9f9b37df7db2df9db4ad9d55”，id：138876   }，
-  {   name : "账1" , PHPSESSID : "dda10dd7e01099508af2214c0989907c" , id : 136715   } ,
-  {   name : "婷" , PHPSESSID : "68414854a18b798a31861659379a4748" , id : 137974   } ,
-  {名称：“365”，PHPSESSID：“68414854a18b798a31861659379a4748”，id：138026   }，
-  {  姓名 ：
+// 账号数组，包含4个账号的信息
+const accounts = [
+  { name: "0210", PHPSESSID: "a68da3417feaaaabcd208edaa2c03e2e", id: 136715 },
+  { name: "婷", PHPSESSID: "711588945e4a1521727346b7941b3d7e", id: 137974 },
+  { name: "365", PHPSESSID: "68414854a18b798a31861659379a4748", id: 138026 },
+  { name: "王博", PHPSESSID: "68414854a18b798a31861659379a4748", id: 146402 },
+  { name: "0819andy819119", PHPSESSID: "09F3A5F9C1B3AFBD60645BD24CE8F840", id: 111264 },
+  { name: "1501andy819117", PHPSESSID: "915365fc93889d5c34143c10b9012d8e", id: 136939 },
+{ name: "6008ailecat", PHPSESSID: "4c77183f9f9b37df7db2df9db4ad9d55", id: 138876 },
+//{ name: "账号44401", PHPSESSID: "token", id: 123456 }
+  //{ name: "账号3", PHPSESSID: "token", id: 123456 },
   //{ name: "爸电信号", PHPSESSID: "token", id: 123456 },
   //{ name: "账号44401", PHPSESSID: "token", id: 123456 }
 
-] ;
+];
 
-//账号1这类可以自己改成自己能区分账号的名字
-//token替换成抓包获得的token id改成账号对应的id
-//不需要的可以用双斜杠来照明账号信息
-// 上面的账号信息不用改，直接改下面的代码
-
-
+//账号1 这类可以自己改成自己能区分账号的名字
+//token  替换成抓包获得的token   id改成账号对应的id
+//不需要的可以用双斜杠来屏蔽账号信息
+//上面的账号信息不用改，直接改下面的代码
 
 
-函数_0xd359  ( _0x55b581 , _0x5c6da3 )  {  const   _0x3ade90 = _0x3ade  (  ) ;返回_0xd359=函数( _0xd35946 , _0x96a123 )  { _0xd35946=_0xd35946- 0xd4 ;让 _ 0x4b4150 =_0x3ade90 [ _0xd35946 ] ;返回_0x4b4150；} , _0xd359  ( _0x55b581, _0x5c6da3 ) ; }  const   _0x79199a =_0xd359; (函数( _0x30a757 , _0x240ba9 )  {  const   _0x5a8e47=_0xd359, _0x299ff9 = _0x30a757   (   ) ; while   ( !! [   ]   )   {尝试{   const    _0x5e8abc =- parseInt   (   _0x5a8e47   (   0xf5   )   ) / 0x1 * ( - parseInt   (   _0x5a8e47   (   0xfb   )   ) / 0x2   ) +- parseInt   (   _0x5a8e47   (   0xf7   )   ) / 0x3 *(  parseInt  (  _0x5a8e47  (  '0xee'  ) )  / 0x4 )  + parseInt (  _0x5a8e47  (  ' 0xeb'  ) ) / 0x5 * ( - parseInt ( _0x5a8e47 ( '0xed' ) ) / 0x6）+ parseInt ( _0x5a8e47 ( '0xf9' ) ) ) / 0x7 + parseInt ( _0x5a8e47 ( 0xd6 ) ) /                    0x8 + - parseInt   (   _0x5a8e47   (   '0xf6'   )   ) / 0x9 + parseInt   (   _0x5a8e47   (   0xd5   )   ) / 0xa ;（如果_0x5e8abc==_0x240ba9 ）打破；否则_0x299ff9 [   '推'   ]   ( _0x299ff9 [   '升降'   ]   (   )   ） ; }   catch   (   _0x5274b5   )   { _0x299ff9 [   '推'   ]   ( _0x299ff9 [   '升降'   ]  (_0x79199a   _0x79199a   ' : 'zh-CN,zh-Hans;q=0.9' } ,过期时间=新日期( '2024-081600:00' ) ,当前时间=新日期( ) ; function  _0x3ade ( ) { const  _0x339b42 = [ '6057lXFDvf' , '1904598ryuUxU' , '18wIVCEG' , '账号\x20 ' , '2044315SpjGbO' , '剧本已过，请派大星重新获取剧本联系。', '2BAusXR' , ' \x20开始申请活动ID \x20 ' , '12965470nNApZH' , '434936SrtOzI' , '退出' , 'PHPSESSID={{your_PHPSESSID}}; \x20\x20 Hm_lpvt_7bcdbebf31e36c3d172be0fdb2bfc51b=1699276302; \x20 Hm_lvt_7bcdbebf31e36c3d172be0fdb2bfc51b=1699011856,1699107620,1699114236,1699276302' , '名称' , ', \x22 ActivityId \x22 :' , '派大星微信号' , 'Cookie' , '推' , '\x0a\x0a账号\x20 ' ,'log','脚本已过期，请联系派大星重新授权。','https://m.16jingxuan.com/dist/','m.16jingxuan.com','replace','application/json,\x20text/plain,\x20*/*','https://m.16jingxuan.com/api/RobOrder/robOrderTrial','\x20申请活动ID\x20 ' , '获取' , 'SDQD6318' , '{ \x22 id \x22 : \x20 ' , 'POST' , '745OECZKD' , 'PHPSESSID' , '20370dAjTPr' , '384248tplHrP' , '长度' , ' \x20 , \x22 TicketId \x22 : \x22\x22 , \ x22CompetTrialImg \x22 :[ \x22\x22 , \x22\x22 ], \x22 RecommendBabyImg \ x22 : \x22 1 \x22 , \x22 enshrineBabyImg \x22 : \x22 1 \x22 , \ x22 shoppingCartImg \x22 : \x22 1 \x22 , \x22 enshrineShopImg \x22 : \x22 1 \x22 , \x22 support_one\x22 : \x22 1 \ x22 , \x22 support_two \x22 : \x22 1 \x22 , \x22 support_thr \x22 : \x22 1 \x22 }' , 'keep-alive' , 'Mozilla/5.0 \x20 (iPhone; \x20 CPU \x20 iPhone \x20 OS \x20 15_3_1 \x20类似于\x20 Mac \x20 OS \x20 X) \x20 AppleWebKit/605.1.15 \x20(KHTML, \x20如\x20 Gecko) \x20 Mobile/15E148 \x20 MicroMessenger/8.0.42(0x18002a32) \x20 NetType/4G \x20 Language/zh_CN' , ' \x20失败：' , 'body'  ] ;_0x3ade =函数(  )  {返回_0x339b42; } ;返回_0x3ade  (  ) ; }  if  (当前时间 > 过期时间) console [  _0x79199a  (  0xdf  )  ]  (  _0x79199a (  '0xe0'  )  ) , $notify  (  _0x79199a  (  '0xdb'  ) , _0x79199a  (  '0xe8'  ) , _0x79199a  (  '0xfa '  )  ) , $完成(  ) ; else  {函数延迟( _0x40abf5 )  {返回新Promise  (  _0x4859f7 => setTimeout  ( _0x4859f7,_0x40abf5 )  ) ; }  }异步函数executeAccountRequest ( _0x33542b , _0x4f33b9 )  {  const   _0x4e00cf =_0x79199a;标头[  _0x4e00cf  (  0xdc  )  ] = 标头[  _0x4e00cf  ( '0xdc '  )  ]  [  _0x4e00cf  (  '0xe3'  )  ]  (  '{{your_PHPSESSID}}' ,_0x33542b [  _0x4e00cf  (  '0xec'  )  ]  ) ; const   _0x387d4a = _0x4e00cf  (  '0xe9'  ) +_0x33542b [  'id'  ]+ _0x4e00cf  (  0xda  ) +_0x4f33b9+ _0x4e00cf  (  0xf0  ) , _0x2e8860 = {  'url' :url, '方法' :方法，'标题'：标题，'正文'：_0x387d4a } ;尝试{  const   _0x30babc =等待$task _0x  4e00cf  (  0xe7  )  ]  ( _0x2e8860 ) ; 控制台[  _0x4e00cf  (  0xdf  )  ]  (  _0x4e00cf  (  0xde  )+_0x33542b [  _0x4e00cf  (  0xd9  )  ] + _ 0x4e00cf（'0xd4'） +_0x4f33b9+ ' \x20 ！ \x0a ' +_0x30babc [  _0x4e00cf  (  0xf4  )  ]  ) ; }  catch  ( _0x484e70 )  {控制台[  '日志'  ]  (  _0x4e00cf  (  0xf8  ) + _0x33542b [  _0x4e00cf  (  0xd9  )  ] + _0x4e00cf (  0xe6  ) +_0x4f33b9+ _0x4e00cf  (  0xf3  )  484e70  ) ; }  }异步函数execcounts (  )  {  const   _0x22794b = _0x79199a, _0x2a19f3 = [  ] ; for  (  let   _0x51fd22 = 0x0 ;_0x51fd22< 账户[  _0x22794b  (  '0xef'  )  ] ;_0x51fd22++ )  {  const   _0x415af0 = 账户[ _0x51fd22 ] ;for  (  let   _0x243151 = 0x0 ;_0x243151<activityIds [  '长度'  ] ;_0x243151++ )  {  const  _0x1f22c7 =activityIds [ _0x243151 ] ;_0x2a19f3 [  _0x22794b  (  0xdd  )  ]  (  executeAccountRequest  ( _ 0x415af0,_0x1f22c7 )  ) ; }  }等待Promise [  'all'  ]  ( _0x2a19f3 ) , $done  (  ) ,process[  _0x22794b  (  '0xd7'  )  ]  (  ) ; }执行账户(  ) ;
+
+
+function _0xd359(_0x55b581,_0x5c6da3){const _0x3ade90=_0x3ade();return _0xd359=function(_0xd35946,_0x96a123){_0xd35946=_0xd35946-0xd4;let _0x4b4150=_0x3ade90[_0xd35946];return _0x4b4150;},_0xd359(_0x55b581,_0x5c6da3);}const _0x79199a=_0xd359;(function(_0x30a757,_0x240ba9){const _0x5a8e47=_0xd359,_0x299ff9=_0x30a757();while(!![]){try{const _0x5e8abc=-parseInt(_0x5a8e47(0xf5))/0x1*(-parseInt(_0x5a8e47(0xfb))/0x2)+-parseInt(_0x5a8e47(0xf7))/0x3*(parseInt(_0x5a8e47('0xee'))/0x4)+parseInt(_0x5a8e47('0xeb'))/0x5*(-parseInt(_0x5a8e47('0xed'))/0x6)+parseInt(_0x5a8e47('0xf9'))/0x7+parseInt(_0x5a8e47(0xd6))/0x8+-parseInt(_0x5a8e47('0xf6'))/0x9+parseInt(_0x5a8e47(0xd5))/0xa;if(_0x5e8abc===_0x240ba9)break;else _0x299ff9['push'](_0x299ff9['shift']());}catch(_0x5274b5){_0x299ff9['push'](_0x299ff9['shift']());}}}(_0x3ade,0x56b5f));const url=_0x79199a('0xe5'),method=_0x79199a('0xea'),headers={'Accept':_0x79199a(0xe4),'Origin':'https://m.16jingxuan.com','Accept-Encoding':'gzip,\x20deflate,\x20br','Cookie':_0x79199a(0xd8),'Content-Type':'application/json;charset=utf-8','Host':_0x79199a(0xe2),'Connection':_0x79199a(0xf1),'User-Agent':_0x79199a(0xf2),'Referer':_0x79199a(0xe1),'Accept-Language':'zh-CN,zh-Hans;q=0.9'},expirationTime=new Date('2999-08-16T24:00:00Z'),currentTime=new Date();function _0x3ade(){const _0x339b42=['6057lXFDvf','1904598ryuUxU','18wIVCEG','账号\x20','2044315SpjGbO','脚本已过期，请联系派大星重新获取脚本。','2BAusXR','\x20开始申请活动ID\x20','12965470nNApZH','434936SrtOzI','exit','PHPSESSID={{your_PHPSESSID}};\x20\x20Hm_lpvt_7bcdbebf31e36c3d172be0fdb2bfc51b=1699276302;\x20Hm_lvt_7bcdbebf31e36c3d172be0fdb2bfc51b=1699011856,1699107620,1699114236,1699276302','name',',\x22activityId\x22:','派大星微信号','Cookie','push','\x0a\x0a账号\x20','log','脚本已过期，请联系派大星重新授权。','https://m.16jingxuan.com/dist/','m.16jingxuan.com','replace','application/json,\x20text/plain,\x20*/*','https://m.16jingxuan.com/api/RobOrder/robOrderTrial','\x20申请活动ID\x20','fetch','SDQD6318','{\x22id\x22:\x20','POST','745OECZKD','PHPSESSID','20370dAjTPr','384248tplHrP','length','\x20,\x22ticketId\x22:\x22\x22,\x22competeTrialImg\x22:[\x22\x22,\x22\x22],\x22recommendBabyImg\x22:\x221\x22,\x22enshrineBabyImg\x22:\x221\x22,\x22shoppingCartImg\x22:\x221\x22,\x22enshrineShopImg\x22:\x221\x22,\x22support_one\x22:\x221\x22,\x22support_two\x22:\x221\x22,\x22support_thr\x22:\x221\x22}','keep-alive','Mozilla/5.0\x20(iPhone;\x20CPU\x20iPhone\x20OS\x2015_3_1\x20like\x20Mac\x20OS\x20X)\x20AppleWebKit/605.1.15\x20(KHTML,\x20like\x20Gecko)\x20Mobile/15E148\x20MicroMessenger/8.0.42(0x18002a32)\x20NetType/4G\x20Language/zh_CN','\x20失败：','body'];_0x3ade=function(){return _0x339b42;};return _0x3ade();}if(currentTime>expirationTime)console[_0x79199a(0xdf)](_0x79199a('0xe0')),$notify(_0x79199a('0xdb'),_0x79199a('0xe8'),_0x79199a('0xfa')),$done();else{function delay(_0x40abf5){return new Promise(_0x4859f7=>setTimeout(_0x4859f7,_0x40abf5));}}async function executeAccountRequest(_0x33542b,_0x4f33b9){const _0x4e00cf=_0x79199a;headers[_0x4e00cf(0xdc)]=headers[_0x4e00cf('0xdc')][_0x4e00cf('0xe3')]('{{your_PHPSESSID}}',_0x33542b[_0x4e00cf('0xec')]);const _0x387d4a=_0x4e00cf('0xe9')+_0x33542b['id']+_0x4e00cf(0xda)+_0x4f33b9+_0x4e00cf(0xf0),_0x2e8860={'url':url,'method':method,'headers':headers,'body':_0x387d4a};try{const _0x30babc=await $task[_0x4e00cf(0xe7)](_0x2e8860);console[_0x4e00cf(0xdf)](_0x4e00cf(0xde)+_0x33542b[_0x4e00cf(0xd9)]+_0x4e00cf('0xd4')+_0x4f33b9+'\x20！\x0a'+_0x30babc[_0x4e00cf(0xf4)]);}catch(_0x484e70){console['log'](_0x4e00cf(0xf8)+_0x33542b[_0x4e00cf(0xd9)]+_0x4e00cf(0xe6)+_0x4f33b9+_0x4e00cf(0xf3)+_0x484e70);}}async function executeAccounts(){const _0x22794b=_0x79199a,_0x2a19f3=[];for(let _0x51fd22=0x0;_0x51fd22<accounts[_0x22794b('0xef')];_0x51fd22++){const _0x415af0=accounts[_0x51fd22];for(let _0x243151=0x0;_0x243151<activityIds['length'];_0x243151++){const _0x1f22c7=activityIds[_0x243151];_0x2a19f3[_0x22794b(0xdd)](executeAccountRequest(_0x415af0,_0x1f22c7));}}await Promise['all'](_0x2a19f3),$done(),process[_0x22794b('0xd7')]();}executeAccounts();
