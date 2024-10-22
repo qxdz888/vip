c735dc46c34111d0c45c5c95b7f7c1a5f15bb4a9
qxdz888 <161924047+qxdz888@users.noreply.github.com>
2024-10-22 19:17:43 +0800

    Update hqtoken.js

--- hqtoken.js
+++ hqtoken.js
@@ -1,18 +1,18 @@
 /**
-* @作者派大星 pdx
-* @function 获取应用程序的 cookie 或令牌通用脚本
-* @日期 2024-02-28 20:30:00
+ * @author 派大星  pdx
+ * @function 获取应用的cookie或token通用脚本
+ * @date 2024-02-28 20:30:00
 获取token
 
-^http:\/\/www\.myy2\.com\/h5\/17\/userCenter url 脚本响应正文
+^http:\/\/www\.myy2\.com\/h5\/17\/userCenter url script-response-body 
  
-*/
+ */
 
-/////////////////////////////////////////////////////////////////
-const  $ = new API ( "获取Cookie或Token通用脚本" ) ;
-const  req_url = $请求。网址；
-const  req_headers = $请求。标头；
-const  req_body = $请求。身体;
+////////////////////////////////
+const $ = new API("获取Cookie或Token通用脚本");
+const req_url = $request.url;
+const req_headers = $request.headers;
+const req_body = $request.body;
 const rsp_body = $response ? $response.body : "{}";
 
 // console.log(`当前请求的url: ${req_url}`);
@@ -57,27 +57,27 @@ function getCookieORToken() {
 function parseDataString(dataString) {
   let data = {};
   // 使用正则表达式匹配换行符号和@符号进行拆分
-  让 parts = dataString.分割( /[\n@]/ ) ;
-  部分。forEach (部分=> {
-    // 对每个部分再根据“&”符号分割为 uid 和 token
-    让 [ uid , token ] = 部分。分裂（”＆” ） ;
-    if  ( uid && 令牌)  {
-      数据[ uid ] = 令牌；
+  let parts = dataString.split(/[\n@]/);
+  parts.forEach(part => {
+    // 对每个部分再根据 "&" 符号拆分为 uid 和 token
+    let [uid, token] = part.split("&");
+    if (uid && token) {
+      data[uid] = token;
     }
-  } ) ;
-  返回数据；
+  });
+  return data;
 }
 
 // 更新数据对象中指定 UID 的 Token
-函数 updateToken ( uidToUpdate , newToken ,数据)  {
-  if  ( data.hasOwnProperty ( uidToUpdate ) ) {​ 
+function updateToken(uidToUpdate, newToken, data) {
+  if (data.hasOwnProperty(uidToUpdate)) {
     // 如果 UID 存在，则更新其对应的 Token
-    数据[ uidToUpdate ] = newToken;
-    安慰。log ( "UID 令牌更新成功：" + uidToUpdate ) ;
-  } 别的 {
-    // 如果 UID 不存在，则添加 UID 和对应的 Token
-    数据[ uidToUpdate ] = newToken;
-    安慰。log ( "新UID和Token添加成功：" + uidToUpdate ) ;
+    data[uidToUpdate] = newToken;
+    console.log("Token updated successfully for UID: " + uidToUpdate);
+  } else {
+    // 如果 UID 不存在，则新增 UID 和对应的 Token
+    data[uidToUpdate] = newToken;
+    console.log("New UID and Token added successfully: " + uidToUpdate);
   }
 }
 
@@ -94,6 +94,6 @@ function convertDataToString(data) {
   return result;
 }
 
-/************************************ API************ **********************/
+/*********************************** API *************************************/
 function ENV() { const e = "undefined" != typeof $task, t = "undefined" != typeof $loon, s = "undefined" != typeof $httpClient && !t, i = "function" == typeof require && "undefined" != typeof $jsbox; return { isQX: e, isLoon: t, isSurge: s, isNode: "function" == typeof require && !i, isJSBox: i, isRequest: "undefined" != typeof $request, isScriptable: "undefined" != typeof importModule } } function HTTP(e = { baseURL: "" }) { const { isQX: t, isLoon: s, isSurge: i, isScriptable: n, isNode: o } = ENV(), r = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&\/\/=]*)/; const u = {}; return ["GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"].forEach(l => u[l.toLowerCase()] = (u => (function (u, l) { l = "string" == typeof l ? { url: l } : l; const h = e.baseURL; h && !r.test(l.url || "") && (l.url = h ? h + l.url : l.url); const a = (l = { ...e, ...l }).timeout, c = { onRequest: () => { }, onResponse: e => e, onTimeout: () => { }, ...l.events }; let f, d; if (c.onRequest(u, l), t) f = $task.fetch({ method: u, ...l }); else if (s || i || o) f = new Promise((e, t) => { (o ? require("request") : $httpClient)[u.toLowerCase()](l, (s, i, n) => { s ? t(s) : e({ statusCode: i.status || i.statusCode, headers: i.headers, body: n }) }) }); else if (n) { const e = new Request(l.url); e.method = u, e.headers = l.headers, e.body = l.body, f = new Promise((t, s) => { e.loadString().then(s => { t({ statusCode: e.response.statusCode, headers: e.response.headers, body: s }) }).catch(e => s(e)) }) } const p = a ? new Promise((e, t) => { d = setTimeout(() => (c.onTimeout(), t(`${u} URL: ${l.url} exceeds the timeout ${a} ms`)), a) }) : null; return (p ? Promise.race([p, f]).then(e => (clearTimeout(d), e)) : f).then(e => c.onResponse(e)) })(l, u))), u } function API(e = "untitled", t = !1) { const { isQX: s, isLoon: i, isSurge: n, isNode: o, isJSBox: r, isScriptable: u } = ENV(); return new class { constructor(e, t) { this.name = e, this.debug = t, this.http = HTTP(), this.env = ENV(), this.node = (() => { if (o) { return { fs: require("fs") } } return null })(), this.initCache(); Promise.prototype.delay = function (e) { return this.then(function (t) { return ((e, t) => new Promise(function (s) { setTimeout(s.bind(null, t), e) }))(e, t) }) } } initCache() { if (s && (this.cache = JSON.parse($prefs.valueForKey(this.name) || "{}")), (i || n) && (this.cache = JSON.parse($persistentStore.read(this.name) || "{}")), o) { let e = "root.json"; this.node.fs.existsSync(e) || this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.root = {}, e = `${this.name}.json`, this.node.fs.existsSync(e) ? this.cache = JSON.parse(this.node.fs.readFileSync(`${this.name}.json`)) : (this.node.fs.writeFileSync(e, JSON.stringify({}), { flag: "wx" }, e => console.log(e)), this.cache = {}) } } persistCache() { const e = JSON.stringify(this.cache, null, 2); s && $prefs.setValueForKey(e, this.name), (i || n) && $persistentStore.write(e, this.name), o && (this.node.fs.writeFileSync(`${this.name}.json`, e, { flag: "w" }, e => console.log(e)), this.node.fs.writeFileSync("root.json", JSON.stringify(this.root, null, 2), { flag: "w" }, e => console.log(e))) } write(e, t) { if (this.log(`SET ${t}`), -1 !== t.indexOf("#")) { if (t = t.substr(1), n || i) return $persistentStore.write(e, t); if (s) return $prefs.setValueForKey(e, t); o && (this.root[t] = e) } else this.cache[t] = e; this.persistCache() } read(e) { return this.log(`READ ${e}`), -1 === e.indexOf("#") ? this.cache[e] : (e = e.substr(1), n || i ? $persistentStore.read(e) : s ? $prefs.valueForKey(e) : o ? this.root[e] : void 0) } delete(e) { if (this.log(`DELETE ${e}`), -1 !== e.indexOf("#")) { if (e = e.substr(1), n || i) return $persistentStore.write(null, e); if (s) return $prefs.removeValueForKey(e); o && delete this.root[e] } else delete this.cache[e]; this.persistCache() } notify(e, t = "", l = "", h = {}) { const a = h["open-url"], c = h["media-url"]; if (s && $notify(e, t, l, h), n && $notification.post(e, t, l + `${c ? "\n多媒体:" + c : ""}`, { url: a }), i) { let s = {}; a && (s.openUrl = a), c && (s.mediaUrl = c), "{}" === JSON.stringify(s) ? $notification.post(e, t, l) : $notification.post(e, t, l, s) } if (o || u) { const s = l + (a ? `\n点击跳转: ${a}` : "") + (c ? `\n多媒体: ${c}` : ""); if (r) { require("push").schedule({ title: e, body: (t ? t + "\n" : "") + s }) } else console.log(`${e}\n${t}\n${s}\n\n`) } } log(e) { this.debug && console.log(`[${this.name}] LOG: ${this.stringify(e)}`) } info(e) { console.log(`[${this.name}] INFO: ${this.stringify(e)}`) } error(e) { console.log(`[${this.name}] ERROR: ${this.stringify(e)}`) } wait(e) { return new Promise(t => setTimeout(t, e)) } done(e = {}) { console.log('done!'); s || i || n ? $done(e) : o && !r && "undefined" != typeof $context && ($context.headers = e.headers, $context.statusCode = e.statusCode, $context.body = e.body) } stringify(e) { if ("string" == typeof e || e instanceof String) return e; try { return JSON.stringify(e, null, 2) } catch (e) { return "[object Object]" } } }(e, t) }
 /*****************************************************************************/
